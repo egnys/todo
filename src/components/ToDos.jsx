4860dfd78561140d5d991df99fa5015ce3f5ef48
Kin-Laptop <flinqwe@gmail.com>
2023-04-29 22:32:32 +0300

    responsive fixes

--- src/components/ToDos.jsx
+++ src/components/ToDos.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ToDoItem from "./ToDoItem";
+import '../styles/App.css'
+
 
 const ToDos = ({todos, remove, change}) => {
     return (
-        <div className="mt-5 width-600 m-auto">
+        <div className="mt-5 m-auto">
             {todos.map((todo, index) => <ToDoItem remove={remove} change={change} number={index + 1} todo={todo} key={todo.id}/>)}
         </div>
     );
